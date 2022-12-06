edd806a2334d901e249663fee4600b92c42d608b
davidou997 <davidou997@gmail.com>
2022-12-05 19:57:05 -0500

    Added playRound function

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
+const move = {
+    rock: 0,
+    paper: 1,
+    scissors: 2,
+}
+
+const roundResult = {
+    WIN: 0,
+    TIE: 1,
+    LOSE: 2,
+}
+
 /**
  * Returns a random integer between 0 (inclusive) to 3 (exclusive)
  * @returns Integer
  */
 function getComputerChoice() {
-    return Math.ceil(Math.random() * 3);;
+    return Math.floor(Math.random() * 3);
+}
+
+/**
+ * Takes the player seleciton and computer selection and returns a string indicating a win or loss for the player
+ * 
+ * @param {Number} playerSelection 
+ * @param {Number} computerSelection 
+ * @returns A number indicating a win, loss, or tie
+ */
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionVal = move[playerSelection.toLowerCase()]
+
+    let val = playerSelectionVal - computerSelection;
+    let result;
+
+    if (val === 0) {
+        result = roundResult.TIE;
+    } else if (val === 1 || val === -2) {
+        result = roundResult.WIN;
+    } else {
+        result = roundResult.LOSE;
+    }
+
+    return result;
 }
