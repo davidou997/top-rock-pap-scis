ff60851aae591d2c3a64adb7f49479bf13d14799
davidou997 <davidou997@gmail.com>
2022-12-05 20:05:41 -0500

    Added check for invalid user inputs

--- app.js
+++ app.js
@@ -26,18 +26,25 @@ function getComputerChoice() {
  * @returns A number indicating a win, loss, or tie
  */
 function playRound(playerSelection, computerSelection) {
-    let playerSelectionVal = move[playerSelection.toLowerCase()]
+    let playerSelectionName = playerSelection.toLowerCase();
+    console.log(playerSelection)
+    console.log(computerSelection)
+    if (playerSelectionName in move) {
+        let playerSelectionVal = move[playerSelection.toLowerCase()]
+        let val = playerSelectionVal - computerSelection;
+        let result;
 
-    let val = playerSelectionVal - computerSelection;
-    let result;
-
-    if (val === 0) {
-        result = roundResult.TIE;
-    } else if (val === 1 || val === -2) {
-        result = roundResult.WIN;
+        if (val === 0) {
+            result = roundResult.TIE;
+        } else if (val === 1 || val === -2) {
+            result = roundResult.WIN;
+        } else {
+            result = roundResult.LOSE;
+        }
+        console.log(result)
+        return result;
     } else {
-        result = roundResult.LOSE;
+        console.log(`You cannot pick the move ${playerSelection}.`)
+        return null;
     }
-
-    return result;
 }
